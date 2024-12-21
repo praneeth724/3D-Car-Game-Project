e297c41eb34fdf685ce9b1c501a6e2e01430afe6
praneeth724 <tmkpraneeth@std.appsc.sab.ac.lk>
2024-12-21 10:07:27 +0530

    Added Rotation and Break to the Car Controller

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4624ad230e4ef6cb5e79faa4b2cfb5091bc5ca35367fed114203136de716580b
-size 870
+oid sha256:ee20e0af306a9b6c5fbd377ace762b0dac318e41acdd1f31d50d6fe7fd7ea9a0
+size 2706
